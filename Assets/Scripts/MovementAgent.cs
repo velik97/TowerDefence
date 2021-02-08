2efa874d5b98f8f5a500fc87e6e171d8cc67bd6b
velikovsky <velikovsky@owlcatgames.com>
2021-02-08 20:04:57 +0300

    Простой скрипт для передвежения юнита

---
+++ Assets/Scripts/MovementAgent.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementAgent : MonoBehaviour
+{
+    [SerializeField]
+    private float m_Speed;
+    [SerializeField]
+    private Vector3 m_Target;
+
+    private const float TOLERANCE = 0.1f;
+
+    void Update()
+    {
+        float distance = (m_Target - transform.position).magnitude;
+        if (distance < TOLERANCE)
+        {
+            return;
+        }
+        
+        Vector3 dir = (m_Target - transform.position).normalized;
+        Vector3 delta = dir * (m_Speed * Time.deltaTime);
+        transform.Translate(delta);
+    }
+}
