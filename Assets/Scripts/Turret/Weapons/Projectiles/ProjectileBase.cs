a3ff7ab4fdbb436a28f61f9c57c5084ba94eddc2
velikovsky <velikovsky@owlcatgames.com>
2021-03-22 18:15:25 +0300

    Создал базовый класс для снаряда

---
+++ Assets/Scripts/Turret/Weapons/Projectiles/ProjectileBase.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Enemy;
+using UnityEngine;
+
+namespace Turret.Weapons.Projectiles
+{
+    public abstract class ProjectileBase<TProjectileAsset> : MonoBehaviour where TProjectileAsset : ProjectileAssetBase
+    {
+        protected TProjectileAsset Asset;
+        protected EnemyData EnemyData;
+
+        public void OnInstantiated(Vector3 origin, Vector3 originForward, EnemyData enemyData, TProjectileAsset asset)
+        {
+            Asset = asset;
+            EnemyData = enemyData;
+            transform.position = origin;
+            transform.forward = originForward;
+        }
+
+        private void Update()
+        {
+            TickApproaching();
+        }
+        
+        private void OnCollisionEnter(Collision other)
+        {
+            if (other.gameObject.CompareTag("Enemy"))
+            {
+                if (other.gameObject.GetComponent<EnemyView>().Data == EnemyData)
+                {
+                    OnCollidedWithEnemy();
+                }
+            }
+        }
+
+        protected abstract void TickApproaching();
+
+        protected abstract void OnCollidedWithEnemy();
+    }
+}
