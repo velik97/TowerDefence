150cddb598687fa00a108e63af3bc8ad0fbfd2cc
velikovsky <velikovsky@owlcatgames.com>
2021-03-22 17:16:07 +0300

    Добавил ссылку на TurretView из оружия

--- Assets/Scripts/Turret/Weapons/Projectiles/TurretProjectileWeapon.cs
+++ Assets/Scripts/Turret/Weapons/Projectiles/TurretProjectileWeapon.cs
@@ -5,15 +5,17 @@ namespace Turret.Weapons.Projectiles
     public class TurretProjectileWeapon : ITurretWeapon
     {
         private TurretProjectileWeaponAsset m_Asset;
+        private TurretView m_View;
         private float m_TimeBetweenShots;
         private float m_MaxShotDistance;
 
         private float m_LastShotTime;
 
-        public TurretProjectileWeapon(TurretProjectileWeaponAsset asset)
+        public TurretProjectileWeapon(TurretProjectileWeaponAsset asset, TurretView view)
         {
             m_Asset = asset;
-            
+            m_View = view;
+
             m_TimeBetweenShots = 1f / m_Asset.RateOfFire;
             m_LastShotTime = -m_TimeBetweenShots;
 
--- Assets/Scripts/Turret/Weapons/TurretWeaponFactory.cs
+++ Assets/Scripts/Turret/Weapons/TurretWeaponFactory.cs
@@ -5,12 +5,12 @@ namespace Turret.Weapons
 {
     public static class TurretWeaponFactory
     {
-        public static ITurretWeapon GetWeapon(TurretWeaponAssetBase turretWeaponAsset)
+        public static ITurretWeapon GetWeapon(TurretWeaponAssetBase turretWeaponAsset, TurretView view)
         {
             switch (turretWeaponAsset)
             {
                 case TurretProjectileWeaponAsset turretProjectileWeaponAsset:
-                    return new TurretProjectileWeapon(turretProjectileWeaponAsset);
+                    return new TurretProjectileWeapon(turretProjectileWeaponAsset, view);
                 default:
                     throw new ArgumentOutOfRangeException(nameof(turretWeaponAsset));
             }
