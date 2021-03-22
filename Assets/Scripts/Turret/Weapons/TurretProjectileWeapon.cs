02367381ba82889a0167e186adb86a8bbd0e10b6
velikovsky <velikovsky@owlcatgames.com>
2021-03-22 16:46:43 +0300

    создал оружие с патронами

---
+++ Assets/Scripts/Turret/Weapons/TurretProjectileWeapon.cs
@@ -0,0 +1,16 @@
+﻿namespace Turret.Weapons
+{
+    public class TurretProjectileWeapon : ITurretWeapon
+    {
+        private TurretProjectileWeaponAsset m_Asset;
+
+        public TurretProjectileWeapon(TurretProjectileWeaponAsset asset)
+        {
+            m_Asset = asset;
+        }
+
+        public void TickShoot()
+        {
+        }
+    }
+}
