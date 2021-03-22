32a1f84116effbc2660649c2c194093129104101
velikovsky <velikovsky@owlcatgames.com>
2021-03-22 18:15:50 +0300

    Реализовал поведение пули

---
+++ Assets/Scripts/Turret/Weapons/Projectiles/Bullet/BulletProjectile.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Turret.Weapons.Projectiles.Bullet
+{
+    public class BulletProjectile : ProjectileBase<BulletProjectileAsset>
+    {
+        protected override void TickApproaching()
+        {
+            Vector3 delta = EnemyData.View.transform.position - transform.position;
+            delta = delta.normalized * Asset.Speed;
+
+            transform.Translate(delta);
+        }
+
+        protected override void OnCollidedWithEnemy()
+        {
+            EnemyData.GetDamage(Asset.Damage);
+        }
+    }
+}
---
+++ Assets/Scripts/Turret/Weapons/Projectiles/Bullet/BulletProjectileAsset.cs
@@ -0,0 +1,22 @@
+﻿using Enemy;
+using UnityEngine;
+
+namespace Turret.Weapons.Projectiles.Bullet
+{
+    public class BulletProjectileAsset : ProjectileAssetBase
+    {
+        [SerializeField]
+        private BulletProjectile m_Bullet;
+
+        public int Damage;
+        public float Speed;
+        
+        public override MonoBehaviour CreateProjectile(Vector3 origin, Vector3 originForward, EnemyData enemyData)
+        {
+            BulletProjectile bullet = Instantiate(m_Bullet);
+            bullet.OnInstantiated(origin, originForward, enemyData, this);
+
+            return bullet;
+        }
+    }
+}
