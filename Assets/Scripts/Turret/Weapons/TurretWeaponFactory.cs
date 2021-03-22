824fb975a810edc799a37b311b1fabf266dc07f8
velikovsky <velikovsky@owlcatgames.com>
2021-03-22 16:47:02 +0300

    создал фабрику для разных оружий

---
+++ Assets/Scripts/Turret/Weapons/TurretWeaponFactory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Turret.Weapons
+{
+    public static class TurretWeaponFactory
+    {
+        public static ITurretWeapon GetWeapon(TurretWeaponAssetBase turretWeaponAsset)
+        {
+            switch (turretWeaponAsset)
+            {
+                case TurretProjectileWeaponAsset turretProjectileWeaponAsset:
+                    return new TurretProjectileWeapon(turretProjectileWeaponAsset);
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(turretWeaponAsset));
+            }
+        }
+    }
+}
