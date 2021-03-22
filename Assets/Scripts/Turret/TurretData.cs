07693f5c5234db579bf1926b54be649e4fdea375
velikovsky <velikovsky@owlcatgames.com>
2021-03-22 18:21:52 +0300

    Создаю оружие при создании туррели

--- Assets/Scripts/Turret/TurretData.cs
+++ Assets/Scripts/Turret/TurretData.cs
@@ -1,4 +1,5 @@
 using Field;
+using Turret.Weapons;
 
 namespace Turret
 {
@@ -6,20 +7,28 @@ namespace Turret
     {
         private Node m_Node;
         private TurretView m_View;
+        private TurretAsset m_Asset;
+        private ITurretWeapon m_Weapon;
 
         public TurretView View => m_View;
 
         public Node Node => m_Node;
 
+
+        public ITurretWeapon Weapon => m_Weapon;
+
         public TurretData(TurretAsset asset, Node node)
         {
             m_Node = node;
+            m_Asset = asset;
         }
 
         public void AttachView(TurretView view)
         {
             m_View = view;
             m_View.AttachData(this);
+
+            m_Weapon = m_Asset.WeaponAsset.GetWeapon(m_View);
         }
     }
 }
