965db864f06a241bdd8acc2e15a7dd04545b2b34
velikovsky <velikovsky@owlcatgames.com>
2021-03-22 18:15:13 +0300

    Создал базовый ассет для снаряда

---
+++ Assets/Scripts/Turret/Weapons/Projectiles/ProjectileAssetBase.cs
@@ -0,0 +1,10 @@
+﻿using Enemy;
+using UnityEngine;
+
+namespace Turret.Weapons.Projectiles
+{
+    public abstract class ProjectileAssetBase : ScriptableObject
+    {
+        public abstract MonoBehaviour CreateProjectile(Vector3 origin, Vector3 originForward, EnemyData enemyData);
+    }
+}
