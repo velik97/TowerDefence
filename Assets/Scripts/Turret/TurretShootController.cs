8da2cc298700e564438391f7202c678504de9f0f
velikovsky <velikovsky@owlcatgames.com>
2021-03-29 14:08:22 +0300

    Создал TurretShootController.cs

---
+++ Assets/Scripts/Turret/TurretShootController.cs
@@ -0,0 +1,23 @@
+﻿using Runtime;
+
+namespace Turret
+{
+    public class TurretShootController : IController
+    {
+        public void OnStart()
+        {
+        }
+
+        public void OnStop()
+        {
+        }
+
+        public void Tick()
+        {
+            foreach (TurretData turretData in Game.Player.TurretDatas)
+            {
+                turretData.Weapon.TickShoot();
+            }
+        }
+    }
+}
