dba4631bcaaff0a26979b4b19e1ee6370d522aa4
velikovsky <velikovsky@owlcatgames.com>
2021-03-29 13:30:02 +0300

    Создал BulletProjectile.cs и BulletProjectileAsset.cs

---
+++ Assets/Scripts/Turret/Weapon/Projectile/BulletProjectile.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Turret.Weapon.Projectile
+{
+    public class BulletProjectile : MonoBehaviour, IProjectile
+    {
+        public void TickProjectile()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool DidHit()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void DestroyProjectile()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Assets/Scripts/Turret/Weapon/Projectile/BulletProjectileAsset.cs
@@ -0,0 +1,13 @@
+﻿using Enemy;
+using UnityEngine;
+
+namespace Turret.Weapon.Projectile
+{
+    public class BulletProjectileAsset : ProjectileAssetBase
+    {
+        public override IProjectile CreateProjectile(Vector3 origin, Vector3 originForward, EnemyData enemyData)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
