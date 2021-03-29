a069b776780d5fda49f807ed7733d4a871837d6b
velikovsky <velikovsky@owlcatgames.com>
2021-03-29 13:42:19 +0300

    Реализовал движение, столкновение и уничтожение пули

--- Assets/Scripts/Turret/Weapon/Projectile/BulletProjectile.cs
+++ Assets/Scripts/Turret/Weapon/Projectile/BulletProjectile.cs
@@ -1,22 +1,50 @@
-﻿using UnityEngine;
+﻿using System;
+using Enemy;
+using UnityEngine;
 
 namespace Turret.Weapon.Projectile
 {
     public class BulletProjectile : MonoBehaviour, IProjectile
     {
+        private float m_Speed;
+        private bool m_DidHit = false;
+        private EnemyData m_HitEnemy = null;
+
+        public void SetSpeed(float speed)
+        {
+            m_Speed = speed;
+        }
+
         public void TickProjectile()
         {
-            throw new System.NotImplementedException();
+            transform.Translate(transform.forward * m_Speed);
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            m_DidHit = true;
+            if (other.CompareTag("Enemy"))
+            {
+                EnemyView enemyView = other.GetComponent<EnemyView>();
+                if (enemyView != null)
+                {
+                    m_HitEnemy = enemyView.Data;
+                }
+            }
         }
 
         public bool DidHit()
         {
-            throw new System.NotImplementedException();
+            return m_DidHit;
         }
 
         public void DestroyProjectile()
         {
-            throw new System.NotImplementedException();
+            // if (m_HitEnemy != null)
+            // {
+            //     m_HitEnemy.Damage();
+            // }
+            Destroy(gameObject);
         }
     }
 }
