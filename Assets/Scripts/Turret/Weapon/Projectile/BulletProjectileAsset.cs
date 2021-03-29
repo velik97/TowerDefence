b67071ca64b08c5089b6f4fe804fed76740b4be8
velikovsky <velikovsky@owlcatgames.com>
2021-03-29 14:15:36 +0300

    Добавил в пуле ссылку на префаб

--- Assets/Scripts/Turret/Weapon/Projectile/BulletProjectileAsset.cs
+++ Assets/Scripts/Turret/Weapon/Projectile/BulletProjectileAsset.cs
@@ -6,9 +6,12 @@ namespace Turret.Weapon.Projectile
     [CreateAssetMenu(menuName = "Assets/Bullet Projectile Asset", fileName = "Bullet Projectile Asset")]
     public class BulletProjectileAsset : ProjectileAssetBase
     {
+        [SerializeField]
+        private BulletProjectile m_BulletProjectile;
+        
         public override IProjectile CreateProjectile(Vector3 origin, Vector3 originForward, EnemyData enemyData)
         {
-            throw new System.NotImplementedException();
+            return Instantiate(m_BulletProjectile, origin, Quaternion.LookRotation(originForward, Vector3.up));
         }
     }
 }
